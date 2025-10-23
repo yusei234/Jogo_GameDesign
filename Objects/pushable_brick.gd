4b844ad26c776ac073ad4494064fdfece6483b4d
Yusei234 <pit.langelot@gmail.com>
2025-10-23 15:55:07 -0300

    Correction of the pushable block

--- Objects/pushable_brick.gd
+++ Objects/pushable_brick.gd
@@ -2,18 +2,20 @@ extends RigidBody2D
 
 const speed = 200
 
-var direction = Vector2(0, 0)
+var direction_moved = Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
-	var target_velocity = direction * speed
+	var target_velocity = direction_moved * speed
 	apply_central_impulse(target_velocity * delta)
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body is CharacterBody2D and !body.kid):
 		print("Enter")
-		var new_value = 1 if body.faced_direction == "right" else -1
-		direction = Vector2(new_value, 0)
+		var direction := Input.get_axis("left", "right") 
+		if direction:
+			var new_value = direction
+			direction_moved = Vector2(new_value, 0)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	direction = Vector2(0, 0)
+	direction_moved = Vector2(0, 0)
